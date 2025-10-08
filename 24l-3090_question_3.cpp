7f7032fc2262d67201fcf2875e89d24515fdb5cd
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 20:45:55 +0500

    refactor: Take the player names out the game loop

--- 24l-3090_question_3.cpp
+++ 24l-3090_question_3.cpp
@@ -4,14 +4,15 @@
 int main() {
     bool run{true};
     
-    do {
-        std::cout << "Enter the name of first player: ";
-        std::string name1{};
-        std::getline(std::cin, name1);
+    std::cout << "Enter the name of first player: ";
+    std::string name1{};
+    std::getline(std::cin, name1);
 
-        std::cout << "Enter the name of the second player: ";
-        std::string name2{};
-        std::getline(std::cin, name2);
+    std::cout << "Enter the name of the second player: ";
+    std::string name2{};
+    std::getline(std::cin, name2);
+    
+    do {
 
         std::cout << "Want to play again yes= 1, no = 0";
         std::cin >> run;
