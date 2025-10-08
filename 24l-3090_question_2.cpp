9b3638f41ab9d638a7f5e1428d445dad0a738ce1
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 16:24:44 +0500

    feat: Add function to select a random number between 1 and 100

--- 24l-3090_question_2.cpp
+++ 24l-3090_question_2.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 #include<string>
+#include<random>
+
+int random_num() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 100);
+
+    int random_number{dist(gen)};
+
+    return random_number;
+}
 
 int main() {
 
