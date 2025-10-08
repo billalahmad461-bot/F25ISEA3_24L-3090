fa497b3352bb7f1bdc56eb436796159773391a1e
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 13:27:21 +0500

    fix: Change the count variable type to double

--- 24l-3090_question_1.cpp
+++ 24l-3090_question_1.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<string>
 
+int strToIntRoundOff(double count) {
+    
+}
+
 int main() {
     bool run{true};
     do {
         std::cout << "How manu strings do you want to check: ";
-        std::string str_count{};
-        std::getline(std::cin, str_count);
+        double count_double{};
+        std::cin >> count_double;
 
         std::cout << "Want to check again yes= 1/no= 0 :";
         std::cin >> run;
