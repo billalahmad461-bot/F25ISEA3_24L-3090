128e17efaf65fe3fb48df5f06b06b95a7cbf47a5
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 20:42:20 +0500

    feat: Add the do-while loop for mgameplay

--- 24l-3090_question_3.cpp
+++ 24l-3090_question_3.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include<string>
 
 int main() {
+    bool run{true};
+    
+    do {
+
+        std::cout << "Want to play again yes= 1, no = 0";
+        std::cin >> run;
+    } while (run);
 
     return 0;
 }
