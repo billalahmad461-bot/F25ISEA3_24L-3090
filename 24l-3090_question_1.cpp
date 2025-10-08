36f4ad62ee2df95f35c06e343360fd2c377e56d8
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 13:37:43 +0500

    feat: Create a Dynamic array of strings for input

--- 24l-3090_question_1.cpp
+++ 24l-3090_question_1.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 #include<string>
+#include<cmath>
 
 int roundOff(double count) {
     if (count <= 0) {
         std::cout << "Error Only +ve number are allowed\n"
                   << " Enter the number again\n";
         return -1; // To idnetify that the number is negatvie
-    } 
+    }
+
+    return static_cast<int>(std::round(count)); // Round to the nearest integer and return
 }
 
 int main() {
@@ -15,6 +18,12 @@ int main() {
         std::cout << "How manu strings do you want to check: ";
         double count_double{};
         std::cin >> count_double;
+
+        int count_int{roundOff(count_double)};
+
+        std::string *strings = new std::string[count_int];
+
+        delete[] strings; // Memory cleanup
 
         std::cout << "Want to check again yes= 1/no= 0 :";
         std::cin >> run;
