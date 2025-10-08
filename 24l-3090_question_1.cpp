160d47611486e7196d77e0c056a701310c40e8f9
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-10-08 12:56:39 +0500

    feat: Add do-while loop for multiple user inputs

--- 24l-3090_question_1.cpp
+++ 24l-3090_question_1.cpp
@@ -2,6 +2,15 @@
 #include<string>
 
 int main() {
+    bool run{true};
+    do {
+        std::cout << "Enter the string to check: ";
+        std::string str;
+        std::getline(std::cin, str);
+
+        std::cout << "Want to check again yes= 1/no= 0 :";
+        std::cin >> run;
+    } while (run);
 
     return 0;
 }
